f869b7077c3b6b7d07532c33cf9b0a284011ec8e
wiellona <117984663+wiellona@users.noreply.github.com>
2023-11-10 10:27:15 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const getElement = (identifier = "") => document.querySelector(identifier);
+
+function verifySubmission() {
+  let subjectElement = getElement("subject").value;
+  let messageElement = getElement("message").value;
+  let nameElement = getElement("name").value;
+
+  if (subjectElement && messageElement && nameElement) {
+    let body = encodeURIComponent("From: " + nameElement + "\n" + messageElement);
+    window.location.href = "mailto:wiellonadarlene@gmail.com?subject=" + subjectElement + "&body=" + body;
+  } else {
+    alert("Please fill in all required fields.");
+  }
+}
+
+getElement("#sendButton").addEventListener("click", verifySubmission);
